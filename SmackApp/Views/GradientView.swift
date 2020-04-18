fc80bb3fd8b1418fb62bfb1129c9e1c578e7fc66
Neha Dixit <neha.dx007@gmail.com>
2020-04-18 00:40:34 -0400

    End of lesson3

---
+++ SmackApp/Views/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  SmackApp
+//
+//  Created by Saurabh Dixit on 4/17/20.
+//  Copyright © 2020 Dixit. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1)
+    {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var bottonColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    // function to set all this and update in views
+    
+   override func layoutSubviews() {
+   let gradientLayer = CAGradientLayer()
+    gradientLayer.colors = [topColor.cgColor, bottonColor.cgColor]
+    gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+    gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+    gradientLayer.frame = self.bounds
+    self.layer.insertSublayer(gradientLayer, at: 0)
+    
+    }
+}
